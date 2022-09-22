b0b85ab276ef179fd63d9bb10f31e8f4d55b2b1a
Obay <ahmad.obay@gmail.com>
2022-09-22 12:29:35 -0700

    Print new line at the end of each line

--- main.go
+++ main.go
@@ -33,14 +33,14 @@ func main() {
 						/* Skip if the file is named correctly already ***********************************************************************************************************/
 						if newFileName == file.Name() {
 							// fmt.Println("Named correctly: " + file.Name() + ". Skipping...")
-							PrintSuccessf("Named correctly: " + file.Name() + ". Skipping...")
+							PrintSuccess("Named correctly: " + file.Name() + ". Skipping...")
 							continue
 						}
 						/* Make sure file with the same name doesn't already exist ***********************************************************************************************/
 						if _, err := os.Stat(newFileName); err == nil {
 							// path/to/whatever exists
 							// fmt.Println("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
-							PrintErrorf("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
+							PrintError("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
 							continue
 						} else if errors.Is(err, os.ErrNotExist) {
 							// path/to/whatever does *not* exist
@@ -51,7 +51,7 @@ func main() {
 									log.Fatal(e)
 								}
 								// fmt.Println("Renamed file from " + file.Name() + " to " + newFileName)
-								PrintWarningf("Renamed file from " + file.Name() + " to " + newFileName)
+								PrintWarning("Renamed file from " + file.Name() + " to " + newFileName)
 							}
 						} else {
 							// File may or may not exist. See err for details.
