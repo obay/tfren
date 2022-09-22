69c155e066a26d284c59e7b1494e1ec90759972d
Obay <ahmad.obay@gmail.com>
2022-09-22 12:47:25 -0700

    Consider first resource block only and skip anything after.

--- main.go
+++ main.go
@@ -26,22 +26,26 @@ func main() {
 			scanner := bufio.NewScanner(f)
 			for scanner.Scan() {
 				line := scanner.Text()
+
+				// If the line is a comment, skip it
+				if strings.HasPrefix(line, "#") {
+					continue
+				}
+
 				if strings.HasPrefix(line, "resource") {
 					fileNameParts := strings.Split(line, " ")
 					if len(fileNameParts) == 4 {
 						newFileName := fileNameParts[0] + "." + strings.Replace(fileNameParts[1], "\"", "", -1) + "." + strings.Replace(fileNameParts[2], "\"", "", -1) + ".tf"
 						/* Skip if the file is named correctly already ***********************************************************************************************************/
 						if newFileName == file.Name() {
-							// fmt.Println("Named correctly: " + file.Name() + ". Skipping...")
 							PrintSuccess("Named correctly: " + file.Name() + ". Skipping...")
-							continue
+							break
 						}
 						/* Make sure file with the same name doesn't already exist ***********************************************************************************************/
 						if _, err := os.Stat(newFileName); err == nil {
 							// path/to/whatever exists
-							// fmt.Println("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
 							PrintError("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
-							continue
+							break
 						} else if errors.Is(err, os.ErrNotExist) {
 							// path/to/whatever does *not* exist
 							// Rename file
@@ -50,7 +54,6 @@ func main() {
 								if e != nil {
 									log.Fatal(e)
 								}
-								// fmt.Println("Renamed file from " + file.Name() + " to " + newFileName)
 								PrintWarning("Renamed file from " + file.Name() + " to " + newFileName)
 							}
 						} else {
@@ -59,6 +62,7 @@ func main() {
 							log.Fatal(err)
 						}
 					}
+					break // skip rest of the file once first resource is found
 				}
 			}
 
