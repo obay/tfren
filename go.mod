7cdd43eadde2809de3e4a05c077919d1eeed33c6
Obay <ahmad.obay@gmail.com>
2024-02-13 19:30:20 -0800

    Update the code to work with all blocks not just resource

--- go.mod
+++ go.mod
@@ -2,10 +2,22 @@ module github.com/obay/tfren
 
 go 1.19
 
-require github.com/fatih/color v1.13.0
+require (
+	github.com/charmbracelet/log v0.3.1
+	github.com/fatih/color v1.16.0
+)
 
 require (
-	github.com/mattn/go-colorable v0.1.9 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,83 +3,123 @@ package main
 import (
 	"bufio"
 	"errors"
-	"log"
 	"os"
 	"strings"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
-	currentdirectory, err := os.Open(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer currentdirectory.Close()
-
-	files, err := currentdirectory.Readdir(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	files := getCurrentDirectoryFiles()
 	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".tf") && !file.IsDir() {
-			/*********************************************************************************************************************************************************************/
-			f, err := os.Open(file.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			scanner := bufio.NewScanner(f)
-			for scanner.Scan() {
-				line := scanner.Text()
-
-				// If the line is a comment, skip it
-				if strings.HasPrefix(line, "#") {
-					continue
-				}
-
-				if strings.HasPrefix(line, "resource") {
-					fileNameParts := strings.Split(line, " ")
-					if len(fileNameParts) == 4 {
-						newFileName := fileNameParts[0] + "." + strings.Replace(fileNameParts[1], "\"", "", -1) + "." + strings.Replace(fileNameParts[2], "\"", "", -1) + ".tf"
-						/* Skip if the file is named correctly already ***********************************************************************************************************/
-						if newFileName == file.Name() {
-							PrintSuccess("Named correctly: " + file.Name() + ". Skipping...")
-							break
-						}
-						/* Make sure file with the same name doesn't already exist ***********************************************************************************************/
-						if _, err := os.Stat(newFileName); err == nil {
-							// path/to/whatever exists
-							PrintError("A file with the name \"" + newFileName + "\" exists already. Unable to rename \"" + file.Name() + "\". Skipping...")
-							break
-						} else if errors.Is(err, os.ErrNotExist) {
-							// path/to/whatever does *not* exist
-							// Rename file
-							if file.Name() != newFileName {
-								f.Close() // close the file before renaming it
-								e := os.Rename(file.Name(), newFileName)
-								if e != nil {
-									log.Fatal(e)
-								}
-								PrintWarning("Renamed file from " + file.Name() + " to " + newFileName)
-							}
-						} else {
-							// File may or may not exist. See err for details.
-							// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
-							log.Fatal(err)
-						}
-					}
-					break // skip rest of the file once first resource is found
-				}
-			}
-
-			// Close the file explicitly, instead of using defer
-			f.Close()
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
-			}
-			/*********************************************************************************************************************************************************************/
+		if isValidFile(file) {
+			renameFileBasedOnContent(file)
 		}
+	}
+}
+
+func getCurrentDirectoryFiles() []os.FileInfo {
+	currentDirectory, err := os.Open(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer currentDirectory.Close()
+
+	files, err := currentDirectory.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
+}
+
+func isValidFile(file os.FileInfo) bool {
+	return strings.HasSuffix(file.Name(), ".tf") && !file.IsDir()
+}
+
+func renameFileBasedOnContent(file os.FileInfo) {
+	f, err := os.Open(file.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// If the line is a comment, skip it
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		prefixes := []string{"resource", "provider", "variable", "terraform", "module", "data"} // Add any other prefixes to this slice
+		matches := false
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(line, prefix) {
+				matches = true
+				break
+			}
+		}
+
+		if matches {
+			newFileName := generateNewFileName(line)
+			if newFileName == file.Name() {
+				PrintSuccess("Named correctly: " + file.Name() + ". Skipping...")
+				break
+			}
+			handleFileRenaming(file.Name(), newFileName)
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generateNewFileName(line string) string {
+	parts := strings.Split(line, " ")
+	parts = removeQuotesFromSlice(parts)
+	if strings.HasPrefix(line, "resource") && len(parts) == 4 {
+		return parts[0] + "." + parts[1] + "." + parts[2] + ".tf"
+	}
+	if strings.HasPrefix(line, "data") && len(parts) == 4 {
+		return parts[0] + "." + parts[1] + "." + parts[2] + ".tf"
+	}
+	if strings.HasPrefix(line, "provider") && len(parts) == 3 {
+		return parts[0] + "." + parts[1] + ".tf"
+	}
+	if strings.HasPrefix(line, "variable") && len(parts) == 3 {
+		return parts[0] + "." + parts[1] + ".tf"
+	}
+	if strings.HasPrefix(line, "module") && len(parts) == 3 {
+		return parts[0] + "." + parts[1] + ".tf"
+	}
+	if strings.HasPrefix(line, "terraform") && len(parts) == 2 {
+		return parts[0] + ".tf"
+	}
+	return ""
+}
+
+func removeQuotesFromSlice(slice []string) []string {
+	for i, s := range slice {
+		slice[i] = strings.Replace(s, "\"", "", -1)
+	}
+	return slice
+}
+
+func handleFileRenaming(oldName, newName string) {
+	if _, err := os.Stat(newName); err == nil {
+		PrintError("A file with the name \"" + newName + "\" exists already. Unable to rename \"" + oldName + "\". Skipping...")
+		return
+	} else if errors.Is(err, os.ErrNotExist) {
+		e := os.Rename(oldName, newName)
+		if e != nil {
+			log.Fatal(e)
+		}
+		PrintWarning("Renamed file from " + oldName + " to " + newName)
+	} else {
+		log.Fatal(err)
 	}
 }
