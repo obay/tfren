4f6de6cf65caa79436a9d0756b58cb06b4e2bd3a
Ahmad Obay <ahmad.obay@gmail.com>
2025-06-30 19:58:48 -0700

    Correctly name the file and remove unused functions.

--- printhelpers.go
+++ printhelpers.go
@@ -2,29 +2,14 @@ package main
 
 import "github.com/fatih/color"
 
-func PrintErrorf(s string) {
-	printer := color.New(color.FgRed)
-	printer.Printf(s)
-}
-
 func PrintError(s string) {
 	printer := color.New(color.FgRed)
 	printer.Printf(s + "\n")
 }
 
-func PrintWarningf(s string) {
-	printer := color.New(color.FgYellow)
-	printer.Printf(s)
-}
-
 func PrintWarning(s string) {
 	printer := color.New(color.FgYellow)
 	printer.Printf(s + "\n")
-}
-
-func PrintSuccessf(s string) {
-	printer := color.New(color.FgGreen)
-	printer.Printf(s)
 }
 
 func PrintSuccess(s string) {
